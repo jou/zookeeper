7905c0420b53a0aadfe5f9aae6515547d1f77193
Jiayong Ou <jiayong.ou@ymc.ch>
2010-08-13 10:08:48 +0200

    Some JavaDocs and rename getHostName() to getHost()

--- src/java/main/org/apache/zookeeper/HostList.java
+++ src/java/main/org/apache/zookeeper/HostList.java
@@ -3,6 +3,9 @@ package org.apache.zookeeper;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A list of hosts to connect to as {@link Host} objects.
+ */
 public class HostList extends ArrayList<Host> {
 	public HostList() {
 		super();
